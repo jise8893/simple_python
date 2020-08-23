06afb583c2e823207dd284b31e3a04559526f3b7
jise8893 <60957575+jise8893@users.noreply.github.com>
2020-08-23 12:06:26 +0900

    Update pypy3.py

--- pypy3.py
+++ pypy3.py
@@ -1,11 +1,11 @@
 import smtplib
 smtp=smtplib.SMTP_SSL('smtp.gmail.com',465)
 smtp.ehlo()
-smtp.login('silanon65@gmail.com','passwd')
+smtp.login('myemail','passwd')
 from email.message import EmailMessage
 msg=EmailMessage()
 msg['Subject']='email_subject'
 msg['from']='myemail'
-msg['To']='silanon65@gmail.com'
+msg['To']='target email'
 msg.set_content('메일본문')
 smtp.send_message(msg)
